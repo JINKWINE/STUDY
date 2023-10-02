e98dbef2de6f5bc743c40a19a17c1c363d8b7c06
양도헌 <133944163+JINKWINE@users.noreply.github.com>
2023-10-02 09:36:26 +0900

    Update 배열 뒤집기.java

--- 프로그래머스/java/배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/java/배열 뒤집기/배열 뒤집기.java
@@ -1,8 +1,8 @@
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] reverse = new int[num_list.length];
+        int[] reverse = new int[num_list.length]; //num_list 길이의 배열 reverse 생성
         for(int i=0; i<=num_list.length-1; i++) {
-            reverse[i] = num_list[num_list.length-1-i];
+            reverse[i] = num_list[num_list.length-i-1]; //reverse 배열 위치에 num_list의 길이에서 reverse의 원소+1만큼을 차감한 위치의 원소를 치환 
         }
         return reverse;
     }
