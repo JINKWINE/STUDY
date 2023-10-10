4ffe998438a17713b57f01fa712bead4b7f16876
양도헌 <133944163+JINKWINE@users.noreply.github.com>
2023-10-10 22:49:55 +0900

    Update and rename 머쓱이보다 키 큰 사람.java to 머쓱이보다 키 큰 사람.java

--- 프로그래머스/java/머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
+++ 프로그래머스/java/머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -2,8 +2,8 @@ class Solution {
     public int solution(int[] array, int height) {
         int answer = 0;
         for(int i = 0; i < array.length; i++){
-            if(array[i] > height){
-                answer++;
+            if(array[i] > height){ //height가 array의 '요소'보다 클 때
+                answer++; //answer의 정수 증가
             }
         }
         return answer;
