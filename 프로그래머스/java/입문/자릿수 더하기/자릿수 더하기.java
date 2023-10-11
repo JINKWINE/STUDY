6a5a0eb27ca7cf1b22ca411c66838d3920ebe035
양도헌 <133944163+JINKWINE@users.noreply.github.com>
2023-10-11 14:57:14 +0900

    Update and rename 프로그래머스/unrated/120906. 자릿수 더하기/자릿수 더하기.java to 프로그래머스/java/입문/자릿수 더하기/자릿수 더하기.java

--- 프로그래머스/java/입문/자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/java/입문/자릿수 더하기/자릿수 더하기.java
@@ -3,8 +3,8 @@ class Solution {
         int answer = 0;
         
         while(n != 0){
-            answer += n % 10;
-            n /= 10;
+            answer += n % 10; //정수를 10으로 나눈 나머지의 합을 누계
+            n /= 10; //정수를 10으로 나눈 몫
         }
             return answer;
     }
