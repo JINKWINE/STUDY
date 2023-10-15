6c55dca20d2e4f9fa8a8d625b201f021dae39ebe
양도헌 <133944163+JINKWINE@users.noreply.github.com>
2023-10-15 23:52:48 +0900

    Update and rename 프로그래머스/1/131528. 나이 정보가 없는 회원 수 구하기/나이 정보가 없는 회원 수 구하기.sql to 프로그래머스/sql/입문/나이 정보가 없는 회원 수 구하기/나이 정보가 없는 회원 수 구하기.sql

---
+++ 프로그래머스/sql/입문/나이 정보가 없는 회원 수 구하기/나이 정보가 없는 회원 수 구하기.sql
@@ -0,0 +1,15 @@
+-- 코드를 입력하세요
+SELECT COUNT(USER_ID) AS USERS
+FROM USER_INFO
+WHERE AGE IS NULL
+
+--note--
+SELECT COUNT(USER_ID) AS USERS 
+FROM USER_INFO 
+WHERE AGE IS NULL:
+--USER_ID를 조회한 후 AGE값이 NULL인 것을 찾기 때문에 AGE 컬럼의 값이 NULL인 것이 조회 가능
+
+SELECT COUNT(AGE) AS USERS 
+FROM USER_INFO 
+WHERE AGE IS NULL:
+--이미 NULL값을 제외한 AGE컬럼의 값들을 조회한 상태에서 NULL값을 조회하려고 하기 때문에 NULL값 조회 불가
