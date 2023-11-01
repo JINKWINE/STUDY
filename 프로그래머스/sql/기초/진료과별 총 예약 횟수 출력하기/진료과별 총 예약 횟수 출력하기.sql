f0d5166664564ba3f893fdca2077de8a09145e85
양도헌 <133944163+JINKWINE@users.noreply.github.com>
2023-11-01 20:42:16 +0900

    Update 진료과별 총 예약 횟수 출력하기.sql

--- 프로그래머스/sql/기초/진료과별 총 예약 횟수 출력하기/진료과별 총 예약 횟수 출력하기.sql
+++ 프로그래머스/sql/기초/진료과별 총 예약 횟수 출력하기/진료과별 총 예약 횟수 출력하기.sql
@@ -1,6 +1,6 @@
 -- 코드를 입력하세요
 SELECT mcdp_cd as "진료과코드"  ,
-       count(*) as "5월예약건수"      
+       count(*) as "5월예약건수" --숫자로 시작하는 별칭은 홑따옴표(')가 아닌 쌍따옴표(")로 감쌀 것
 FROM appointment
 WHERE to_char(apnt_ymd, 'mm') = '05'
 GROUP BY mcdp_cd
