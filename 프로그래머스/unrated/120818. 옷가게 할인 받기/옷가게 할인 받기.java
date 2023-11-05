f6b2cbc37b53bd81a7de0061a6ea49da1b6fdd36
양도헌 <133944163+JINKWINE@users.noreply.github.com>
2023-11-05 18:50:05 +0900

    [level unrated] Title: 옷가게 할인 받기, Time: 0.04 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int price) {
+        int answer = 0;
+        
+        if (price >= 500000) {
+            answer = (int) (price * 0.8);  // 20% 할인
+        } else if (price >= 300000) {
+            answer = (int) (price * 0.9);  // 10% 할인
+        } else if (price >= 100000) {
+            answer = (int) (price * 0.95);  // 5% 할인
+        } else {
+            answer = price;  // 할인 대상이 아닌 경우 가격 유지
+        }
+        
+        return answer;
+    }
+}
