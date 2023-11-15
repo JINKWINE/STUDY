c4009bb52da184d2fd1affe3dc8834f02b45dccf
양도헌 <133944163+JINKWINE@users.noreply.github.com>
2023-11-15 11:59:52 +0900

    Update 보호소에서 중성화한 동물.sql

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -5,5 +5,17 @@ SELECT ai.animal_id,
 FROM animal_ins ai, animal_outs ao
 WHERE ai.animal_id = ao.animal_id AND
       ai.sex_upon_intake LIKE 'Intact%' AND
+      (ao.sex_upon_outcome LIKE 'Spayed%' OR
+      ao.sex_upon_outcome LIKE 'Neutered%')
+ORDER BY ai.animal_id      
+
+
+--조금 더 보기 편하게 수정할 수 있었음
+SELECT ai.animal_id,
+       ai.animal_type,
+       ai.name
+FROM animal_ins ai, animal_outs ao
+WHERE ai.animal_id = ao.animal_id AND
+      ai.sex_upon_intake LIKE 'Intact%' AND
       ao.sex_upon_outcome NOT LIKE 'Intact%'
-ORDER BY ai.animal_id
+ORDER BY ai.animal_id        
