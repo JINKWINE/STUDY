f6ef48c46bcc96e0ab38fc2f3bbb3f4069de8a10
양도헌 <133944163+JINKWINE@users.noreply.github.com>
2023-11-18 00:32:37 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -0,0 +1,12 @@
+-- 코드를 입력하세요
+SELECT animal_id,
+       name,
+       sex_upon_intake
+FROM animal_ins
+WHERE name LIKE 'Lucy' OR 
+      name LIKE 'Ella' OR 
+      name LIKE 'Pickle' OR
+      name LIKE 'Rogan' OR
+      name LIKE 'Sabrina' OR
+      name LIKE 'Mitty'
+ORDER BY animal_id      
