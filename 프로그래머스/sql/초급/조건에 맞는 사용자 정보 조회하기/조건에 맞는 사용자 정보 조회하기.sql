fafb5072570e632824ad2c7870895d4e153cc95e
양도헌 <133944163+JINKWINE@users.noreply.github.com>
2023-11-30 20:33:40 +0900

    Update 조건에 맞는 사용자 정보 조회하기.sql

--- 프로그래머스/sql/초급/조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/sql/초급/조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -11,3 +11,19 @@ WHERE uu.user_id IN (SELECT writer_id
                      HAVING count(writer_id) >= 3)
 ORDER BY uu.user_id DESC
 
+
+
+-- 처음 작성했던 오답--
+-- SELECT 
+--     uu.user_id,
+--     uu.nickname,
+--     uu.city || '' || uu.street_address1 || '' || uu.street_address2 AS 전체주소,
+--     SUBSTR(uu.tlno, 1, 3) || '-' || SUBSTR(uu.tlno, 4, 4) || '-' || SUBSTR(uu.tlno, 8) AS 전화번호
+-- FROM used_goods_board ub JOIN used_goods_user uu
+-- ON uu.user_id = ub.writer_id
+-- GROUP BY uu.user_id, uu.nickname, uu.city, uu.street_address1, uu.street_address2, uu.tlno
+-- HAVING COUNT(ub.board_id) >= 3  //거래 게시물이 3건인 사용자를 조회
+-- ORDER BY uu.user_id DESC
+
+-- user_id 와 writer_id 를 기준으로 조인 한 후에 게시물 개수를 카운트 해도 게시물 작성자를 조회할 수 없었음
+-- 그래서 user_id의 옵션 안에서 작성자의 수(writer_id)를 세는 방법으로 쿼리 수정
