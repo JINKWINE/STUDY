4eb62d0dce60f436f7ac8e0b4bf163bcdb3951e0
양도헌 <133944163+JINKWINE@users.noreply.github.com>
2023-12-09 16:33:26 +0900

    Update 문자열 곱하기.java

--- 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.java
+++ 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.java
@@ -1,9 +1,22 @@
+//반복문 사용
 class Solution {
     public String solution(String my_string, int k) {
         String answer = "";
         for(int i = 0; i < k; i++){
             answer += my_string;
         }
+        return answer;
+    }
+}
+
+
+
+//repeat 함수 사용
+class Solution {
+    public String solution(String my_string, int k) {
+        String answer = "";
+        answer = my_string.repeat(k);
+    
         return answer;
     }
 }
