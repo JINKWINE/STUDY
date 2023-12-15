5a0d8c68027c4721d82bfe3d52d2850e7a65a6e0
양도헌 <133944163+JINKWINE@users.noreply.github.com>
2023-12-16 08:27:46 +0900

    [level unrated] Title: 정수 찾기, Time: 0.02 ms, Memory: 80.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] num_list, int n) {
+        int answer = 0;
+        for(int num : num_list){
+             if(num == n){
+                answer = 1;
+                break;
+                 
+             }else{
+                answer = 0;
+             } 
+        }
+      
+        return answer;
+    }
+}
