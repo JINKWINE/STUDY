11a943d8f3a126c5f76ab82259aa903cc89f896f
양도헌 <133944163+JINKWINE@users.noreply.github.com>
2023-12-23 01:40:12 +0900

    [level unrated] Title: 수 조작하기 2, Time: 9.55 ms, Memory: 90.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(int[] numLog) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < numLog.length; i++) {
+            int diff = i > 0 ? numLog[i] - numLog[i - 1] : numLog[i];
+
+            if (diff == 1) {
+                result.append("w");
+            } else if (diff == -1) {
+                result.append("s");
+            } else if (diff == 10) {
+                result.append("d");
+            } else if (diff == -10) {
+                result.append("a");
+            }
+        }
+
+        return result.toString();
+    }
+
+}
