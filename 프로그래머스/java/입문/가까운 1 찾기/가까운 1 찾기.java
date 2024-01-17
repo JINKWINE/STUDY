cd6c2d9554c54684af7c857463fbd8e21afa5ce7
양도헌 <133944163+JINKWINE@users.noreply.github.com>
2024-01-17 16:41:39 +0900

    Update and rename 프로그래머스/Lv.0/181898. 가까운 1 찾기/가까운 1 찾기.java to 프로그래머스/java/입문/가까운 1 찾기/가까운 1 찾기.java

--- 프로그래머스/java/입문/가까운 1 찾기/가까운 1 찾기.java
+++ 프로그래머스/java/입문/가까운 1 찾기/가까운 1 찾기.java
@@ -19,3 +19,22 @@ class Solution {
         return answer;
     }
 }
+
+//문제에서 원하는 건 주어진 idx보다 큰 인덱스가 조건이었으니 
+//굳이 0부터 시작하지 않고 idx부터 인덱스를 찾아도 됐기 때문에 아래와 같이 풀이하는 것이 더 깔끔함
+
+class Solution {
+    public int solution(int[] arr, int idx) {
+        int answer = 0;
+        for(int i = idx; i< arr.length;i++){
+           if(arr[i]==1){
+               answer = i;
+               break;
+           }else if(arr[i]!=1){
+               answer = -1;
+           }
+        }
+
+        return answer;
+    }
+}
