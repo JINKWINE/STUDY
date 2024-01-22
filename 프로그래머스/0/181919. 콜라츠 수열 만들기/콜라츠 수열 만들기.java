19bdceac59af586c355b7639cf900bb20185c8a5
양도헌 <133944163+JINKWINE@users.noreply.github.com>
2024-01-22 17:33:54 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.03 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class Solution {
+class Solution {
     public ArrayList<Integer> solution(int n) {
         ArrayList<Integer> answer = new ArrayList<>();
         answer.add(n);
