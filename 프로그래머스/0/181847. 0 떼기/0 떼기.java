0ea0b8c5bcc18f6d9acdb13714e4ceca472b5d1a
양도헌 <133944163+JINKWINE@users.noreply.github.com>
2024-01-22 17:02:16 +0900

    [level 0] Title: 0 떼기, Time: 0.03 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        int toNum = Integer.parseInt(n_str);
+        answer = Integer.toString(toNum);
+        return answer;
+    }
+}
