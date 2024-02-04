4e506d77be5deae0e378df39031966aea46a7f8e
양도헌 <133944163+JINKWINE@users.noreply.github.com>
2024-02-04 15:55:39 +0900

    Update 9로 나눈 나머지.java

--- 프로그래머스/java/입문/9로 나눈  나머지/9로 나눈 나머지.java
+++ 프로그래머스/java/입문/9로 나눈  나머지/9로 나눈 나머지.java
@@ -1,12 +1,12 @@
 class Solution {
     public int solution(String number) {
-        int answer = 0;
+        int answer = 0; 
         int sum = 0;
-        for(int i = 0; i < number.length(); i++){
-            sum += Character.getNumericValue(number.charAt(i));
+        for(int i = 0; i < number.length(); i++){ //문자열을 순환하면서
+            sum += Character.getNumericValue(number.charAt(i)); //getNumericValue를 이용하여 문자열을 정수로 변환한 뒤 합계에 누적 합산
         }
         
-        answer = sum % 9;
+        answer = sum % 9; //각 자리 합산 값을 9로 나눈 나머지를 answer에 할당
         
         return answer;
     }
