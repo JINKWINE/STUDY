63574e0e440b752c7b3aff079e28b542c1236202
양도헌 <133944163+JINKWINE@users.noreply.github.com>
2024-02-04 15:58:01 +0900

    Update 9로 나눈 나머지.java

--- 프로그래머스/java/입문/9로 나눈  나머지/9로 나눈 나머지.java
+++ 프로그래머스/java/입문/9로 나눈  나머지/9로 나눈 나머지.java
@@ -11,3 +11,19 @@ class Solution {
         return answer;
     }
 }
+
+
+//ASCII값을 활용한 정답 도출
+class Solution {
+    public int solution(String number) {
+        int answer = 0;
+
+        for(int i=0; i<number.length(); i++) { //문자열을 순환하면서
+            answer += number.charAt(i) - '0'; //숫자로 된 문자열에서 문자'0'을 뺄셈 연산해 정수로 변환하고 누적값 할당
+        }
+
+        answer %= 9;
+
+        return answer;
+    }
+}
