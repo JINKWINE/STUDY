58c719843069a0f2fd8bc657c6ca1f949ea22c65
양도헌 <133944163+JINKWINE@users.noreply.github.com>
2024-02-07 18:19:06 +0900

    Update 수열과 구간 쿼리(1).java

--- 프로그래머스/java/입문/수열과 구간 쿼리/수열과 구간 쿼리(1)/수열과 구간 쿼리(1).java
+++ 프로그래머스/java/입문/수열과 구간 쿼리/수열과 구간 쿼리(1)/수열과 구간 쿼리(1).java
@@ -1,12 +1,12 @@
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-        for(int[] currentQuery : queries) {
-            int start = currentQuery[0];
-            int end = currentQuery[1];
+        for(int[] currentQuery : queries) { //쿼리를 순환하면서
+            int start = currentQuery[0]; //첫 번째 배열 첫 번째 원소를 start
+            int end = currentQuery[1]; //첫 번째 배열 두 번째 원소를 end라고 정의
             
-            for(int i = start; i <= end; i++) {
+            for(int i = start; i <= end; i++) { //주어진 배열의 처음부터 끝까지 순환하면서
                 
-                arr[i] += 1;
+                arr[i] += 1; //배열의 원소값이 일치하면 1을 더한다
             }
         }
         
