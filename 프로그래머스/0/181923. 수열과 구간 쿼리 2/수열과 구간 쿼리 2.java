afffcf61f19e4a71b9dbc3a75c2651799ee6f232
양도헌 <133944163+JINKWINE@users.noreply.github.com>
2024-02-08 15:14:52 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 1.82 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -1,24 +1,24 @@
 import java.util.Arrays;
 
 class Solution {
-    public int[] solution(int[] arr, int[][] queries) {
+    public int[] solution(int[] arr, int[][] queries){
         int[] answer = new int[queries.length];
 
-        for (int q = 0; q < queries.length; q++) {
-            int start = queries[q][0];
-            int end = queries[q][1];
-            int comparison = queries[q][2];
+        for(int i = 0; i < queries.length; i++){
+           int start = queries[i][0];
+           int end = queries[i][1];
+           int comparison = queries[i][2];
 
-            int minInRange = Integer.MAX_VALUE; // 현재까지의 최솟값을 저장하는 변수
+           int minInRange = Integer.MAX_VALUE; // 현재까지의 최솟값을 저장하는 변수
 
-            for (int i = start; i <= end; i++) {
-                if (arr[i] > comparison && arr[i] < minInRange) {
-                    minInRange = arr[i];
+           for(int j = start; j <= end; j++){
+                if(arr[j] > comparison && arr[j] < minInRange){
+                    minInRange = arr[j];
                 }
-            }
+           }
 
             // 현재 쿼리에 대한 결과를 answer 배열에 저장
-            answer[q] = (minInRange == Integer.MAX_VALUE) ? -1 : minInRange;
+           answer[i] = (minInRange == Integer.MAX_VALUE) ? -1 : minInRange;
         }
 
         return answer;
