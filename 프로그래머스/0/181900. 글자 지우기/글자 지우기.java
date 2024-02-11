0210d3e9b4d0448ae7499e3f9896092251d38e6f
양도헌 <133944163+JINKWINE@users.noreply.github.com>
2024-02-11 23:33:34 +0900

    Update 글자 지우기.java

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 class Solution {
     public String solution(String my_string, int[] indices) {
-        Arrays.sort(indices);
+        Arrays.sort(indices); //배열 정렬
         StringBuilder sb = new StringBuilder(my_string);
         for (int i = indices.length - 1; i >= 0; i--) {
             int index = indices[i];
