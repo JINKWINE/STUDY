33fe7050af19c889d5c7a8c919550a70917cd828
양도헌 <133944163+JINKWINE@users.noreply.github.com>
2024-02-15 23:03:10 +0900

    Update 배열 만들기(2).java

--- 프로그래머스/java/입문/배열 만들기/배열 만들기(2)/배열 만들기(2).java
+++ 프로그래머스/java/입문/배열 만들기/배열 만들기(2)/배열 만들기(2).java
@@ -2,10 +2,10 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int l, int r) {
-        List<Integer> list = new ArrayList<>(); //
-        int count = 0;
-        for(int i = l; i <= r; i++){
-            String str = i + "";
+        List<Integer> list = new ArrayList<>(); //정수 리스트 생성
+        int count = 0; 
+        for(int i = l; i <= r; i++){ //정수 l부터 r까지 순회
+            String str = i + ""; //
             for(int j = 0; j < str.length(); j++){
                 if(str.charAt(j) == 48 || str.charAt(j) == 53){
                     count ++;
