9dd8fd300254458a24e64d6bab1bf2c36c6aabad
양도헌 <133944163+JINKWINE@users.noreply.github.com>
2024-03-25 15:02:47 +0900

    [level 0] Title: 세 개의 구분자, Time: 74.11 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/README.md
+++ 프로그래머스/0/181862. 세 개의 구분자/README.md
@@ -1,10 +1,10 @@
 # [level 0] 세 개의 구분자 - 181862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181862#) 
 
 ### 성능 요약
 
-메모리: 104 MB, 시간: 71.05 ms
+메모리: 104 MB, 시간: 74.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 25일 14:48:01
+2024년 03월 25일 15:03:01
 
 ### 문제 설명
 
--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -1,9 +1,12 @@
 class Solution {
     public String[] solution(String myStr) {
         myStr = myStr.replaceAll("[a|b|c]+", ",");
-        myStr = myStr.charAt(0) == ','? myStr.substring(1) : myStr;
-        myStr = myStr.equals("")? "EMPTY" : myStr;
-        
+        if(myStr.startsWith(",")) {
+            myStr = myStr.substring(1);
+        }
+        if(myStr.equals("")) {
+            myStr = "EMPTY";
+        }
         return myStr.split("[,]");
     }
 }
