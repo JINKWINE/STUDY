17c4951ffbb5ea6fea7b87b6490b0b46fbcbd641
양도헌 <133944163+JINKWINE@users.noreply.github.com>
2024-04-08 22:55:43 +0900

    [level 0] Title: 2의 영역, Time: 2.48 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.java
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -1,34 +1,34 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-
 class Solution {
     public int[] solution(int[] arr) {
-        int start = -1; //2가 한 번도 안 나올 경우
-        int end = 0; //2가 한 번 나온 후 갱신
+        int start = -1;
+        int end = 0;
         
-        for(int i = 0; i < arr.length; i++){ //arr을 순회하면서
-            if(arr[i] == 2) { //원소의 값이 2라면
-                start = i; //start에 인덱스 저장
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == 2) {
+                start = i;
                 break;
             }
         }
         
-        if(start != -1) { //2가 한 번 나오고 
-            for(int i = start; i < arr.length; i++) { //처음 2가 나온 인덱스부터 arr을 순회
-                if(arr[i] == 2) { //원소의 값이 2일 때
-                    end = i; //end에 해당 인덱스 저장
+        if(start != -1) {
+            for(int i = start; i < arr.length; i++) {
+                if(arr[i] == 2) {
+                    end = i;
                 }
             }
         }
         
-        if(start != -1) { //2가 한 번 나오고
-            int[] answer = new int[end - start + 1]; //2에서 2까지의 길이만큼 정답 배열 초기화
-            for(int i = 0; i < answer.length; i++) { //정답 배열 길이만큼 순회
-                answer[i] = arr[start + i]; //처음 2가 나오는 인덱스부터 정답 배열에 복사
+        if(start != -1) {
+            int[] answer = new int [end - start + 1];
+            for(int i = 0; i < answer.length; i++) {
+                answer[i] = arr[i + start];
             }
+            
             return answer;
+            
         } else {
-            return new int[]{-1}; //arr에 2가 없는 경우
+            return new int[]{-1};   
         }
+        
     }
 }
